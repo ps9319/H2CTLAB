06a5527c6c783e2277f54054c46c61041065d70a
myon-burger <handj25@gamil.com>
2025-11-05 16:49:06 +0900

    풍류&예술 씬 수정

---
+++ Assets/Editor/TimeScaleToolbarWindow.cs
@@ -0,0 +1,35 @@
+using UnityEditor;
+using UnityEngine;
+
+public class TimeScaleToolbarWindow
+{
+    // 단축키: Ctrl+Q → 2x <-> 1x 토글
+    [MenuItem("Tools/TimeScale/2x Toggle %q")]
+    private static void ToggleDoubleTimeScale()
+    {
+        if (Time.timeScale >= 2.0f)
+        {
+            Time.timeScale = 1.0f;
+        }
+        else
+        {
+            Time.timeScale = 2.0f;
+        }
+        Debug.Log($"Current TimeScale: {Time.timeScale:0.00}");
+    }
+
+    // 단축키: Ctrl+W → 1x <-> 0.5x 토글
+    [MenuItem("Tools/TimeScale/0.5x Toggle %w")]
+    private static void ToggleHalfTimeScale()
+    {
+        if (Time.timeScale <= 0.5f)
+        {
+            Time.timeScale = 1.0f;
+        }
+        else
+        {
+            Time.timeScale = 0.5f;
+        }
+        Debug.Log($"Current TimeScale: {Time.timeScale:0.00}");
+    }
+}
