822f177d7b745ff4c6c4c40eaea7c8871b66b9c9
myon-burger <handj25@gamil.com>
2025-11-11 12:14:18 +0900

    예술 씬 수정

--- Assets/Project/Scripts/ApplyParentVFXTexture.cs
+++ Assets/Project/Scripts/ApplyParentVFXTexture.cs
@@ -3,21 +3,40 @@ using UnityEngine.VFX;
 
 public class ApplyParentVFXTexture : MonoBehaviour
 {
+    public enum SourceMode
+    {
+        Parent,
+        AssignedObject
+    }
+
+    [Header("Source Settings")]
+    public SourceMode sourceMode = SourceMode.Parent;
+    public VisualEffect sourceVFX; // AssignedObject 모드일 때 사용
+
     public string texturePropertyName = "Texture"; // 노출 프로퍼티 이름
 
     void Start()
     {
-        var parent = transform.parent;
-        if (parent == null) return;
+        VisualEffect src = null;
 
-        var parentVFX = parent.GetComponent<VisualEffect>();
+        if (sourceMode == SourceMode.Parent)
+        {
+            var parent = transform.parent;
+            if (parent != null)
+                src = parent.GetComponent<VisualEffect>();
+        }
+        else if (sourceMode == SourceMode.AssignedObject)
+        {
+            src = sourceVFX;
+        }
+
         var myVFX = GetComponent<VisualEffect>();
 
-        if (parentVFX != null && myVFX != null)
+        if (src != null && myVFX != null)
         {
-            if (parentVFX.HasTexture(texturePropertyName))
+            if (src.HasTexture(texturePropertyName))
             {
-                Texture parentTexture = parentVFX.GetTexture(texturePropertyName);
+                Texture parentTexture = src.GetTexture(texturePropertyName);
                 myVFX.SetTexture(texturePropertyName, parentTexture);
             }
         }
